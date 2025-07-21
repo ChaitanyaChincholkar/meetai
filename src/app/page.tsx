e28b5f77a8c90ed5d87fc21edd77162be5f8e82f
ChaitanyaChincholkar <chaitanyachincholkar433@gmail.com>
2025-07-21 12:40:02 +0530

    05: authentication socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,16 @@
-"use client";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
+import { HomeView } from "@/modules/home/ui/views/home-view";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-
-export default function Home() {
-
-   const {data: session} = authClient.useSession() 
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState(""); 
-
-  const onSubmit = () => {
-    authClient.signUp.email({
-      name,
-      email,
-      password,
-    },{
-      onError: () => {
-        window.alert("Somthing went wrong, please try again.");
-      },
-      onSuccess: () => {
-        window.alert("User created successfully!");
-      }
-    });
+const Page = async() => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if(!session){
+    redirect("/sign-in");
   }
-
-   const onLogin = () => {
-    authClient.signIn.email({
-      email,
-      password,
-    },{
-      onError: () => {
-        window.alert("Somthing went wrong, please try again.");
-      },
-      onSuccess: () => {
-        window.alert("User created successfully!");
-      }
-    });
-  }
-
-  if(session) {
-    return (
-      <div className="flex flex-col p-4 gap-y-4">
-        <h1 className="text-2xl font-bold">Welcome, {session.user.name}!</h1>
-        <p className="mt-2">You are logged in as {session.user.email}.</p>
-        <Button onClick={() => authClient.signOut()}>
-          Sign Out
-        </Button>
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex flex-col gap-y-10">
-    <div className="p-4 flex flex-col gap-y-4">
-      <Input placeholder="Name" 
-      value={name}
-      onChange={(e) => setName(e.target.value)} 
-      />
-      <Input placeholder="Email" 
-      value={email} 
-      onChange={(e) => setEmail(e.target.value)} 
-      />
-      <Input placeholder="Password" 
-      type="password" 
-      value={password} 
-      onChange={(e) => setPassword(e.target.value)} 
-      />
-      <Button onClick={onSubmit}>
-        Create User
-      </Button>
-    </div>
-
-     <div className="p-4 flex flex-col gap-y-4">
-
-      <Input placeholder="Email" 
-      value={email} 
-      onChange={(e) => setEmail(e.target.value)} 
-      />
-      <Input placeholder="Password" 
-      type="password" 
-      value={password} 
-      onChange={(e) => setPassword(e.target.value)} 
-      />
-      <Button onClick={onLogin}>
-        Login User
-      </Button>
-    </div>
-   </div>
-  );
-};
+  return <HomeView />;
+}
+ 
+export default Page;
--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { OctagonAlertIcon } from "lucide-react";
+import {FaGithub, FaGoogle } from "react-icons/fa";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { authClient } from "@/lib/auth-client";
@@ -29,6 +30,7 @@ const  formSchema = z.object({
 
 export const SignUpView = () => {
     const router = useRouter();
+
     const [pending, setPending] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
@@ -49,6 +51,7 @@ const onSubmit =  (data: z.infer<typeof formSchema>) => {
             name: data.name,
             email: data.email,
             password: data.password,
+            callbackURL: "/",
         },
         {
             onSuccess: () => {
@@ -56,12 +59,31 @@ const onSubmit =  (data: z.infer<typeof formSchema>) => {
                 router.push("/");
             },
             onError: ({error}) => {
+                setPending(false);
                 setError(error.message);
             },
         }
     ); 
 };
-            
+    const onSocial =  (provider: "github" | "google") => {
+        setError(null);
+        setPending(true);
+      authClient.signIn.social(
+        {
+           provider:provider,
+           callbackURL:"/",
+        },
+        {
+            onSuccess: () => {
+                setPending(false);
+            },
+            onError: ({error}) => {
+                setPending(false);
+                setError(error.message);
+            },
+        }
+    ); 
+};        
 
   return (
     <div className="flex flex-col gap-6">
@@ -178,11 +200,22 @@ const onSubmit =  (data: z.infer<typeof formSchema>) => {
                             </span>
                         </div>
                         <div className="grid grid-cols-2 gap-4">
-                            <Button disabled={pending} variant="outline" type="button" className="w-full">
-                                Google
+                            <Button 
+                            disabled={pending}
+                            onClick={() => onSocial("google")}
+                            variant="outline" 
+                            type="button" 
+                            className="w-full"
+                            >
+                                <FaGoogle/>
                             </Button>
-                            <Button disabled={pending} variant="outline" type="button" className="w-full">
-                                GitHub
+                            <Button 
+                            disabled={pending}
+                            onClick={() => onSocial("github")}
+                            variant="outline" 
+                            type="button" 
+                            className="w-full">
+                                <FaGithub/>
                             </Button>
                         </div>
                         <div className="text-center text-sm">
