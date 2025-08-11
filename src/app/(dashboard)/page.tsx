9aa4f9a889e465d595668e37fe9336a5b8fe3aa2
ChaitanyaChincholkar <chaitanyachincholkar433@gmail.com>
2025-08-11 14:36:03 +0530

    08: trpc setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth";
 import { HomeView } from "@/modules/home/ui/views/home-view";
 
+
 const Page = async() => {
   const session = await auth.api.getSession({
     headers: await headers(),
@@ -10,6 +11,7 @@ const Page = async() => {
   if(!session){
     redirect("/sign-in");
   }
+
   return <HomeView />;
 }
  
